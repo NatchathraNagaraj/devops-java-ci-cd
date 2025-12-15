ab71a6c7bc293b5643769506ab48a07075e3464a
Natchathra Nagaraj <natchathranagaraj@gmail.com>
2025-12-15 07:12:54 +0000

    Complete end-to-end DevOps CI/CD project with monitoring and Swagger UI

--- healthapp/src/main/java/com/example/healthapp/HealthappApplication.java
+++ healthapp/src/main/java/com/example/healthapp/HealthappApplication.java
@@ -1,4 +1,4 @@
-package com.devops.healthapp;
+package com.example.healthapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HealthappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HealthappApplication.class, args);
+    }
 }
